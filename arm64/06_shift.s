e93d061f27bdabef586867df2a9cced9ff5ede7d
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-06-12 01:56:49 -0600

    update shift and slope

---
+++ arm64/06_shift.s
@@ -0,0 +1,47 @@
+/*
+
+    Print if digit is odd or even
+    Luis Espino 2024
+
+    output:
+    [print 1-odd or 0-even from stdin]
+
+*/
+
+.global _start
+
+.data
+out: .ascii " - - - \n"
+
+.text
+_start:
+    ldr x1, =out        // load output
+
+    mov w0, 0b1001      // 9 = 1001
+    asr w0, w0, 2       // w0 >> 2
+    add w0, w0, 48      // adjust ascii
+    strb w0, [x1]       // store 2 = 0010
+
+    mov w0, 0b10000     // 16 = 10000
+    lsr w0, w0, 1       // w0 >> 1
+    add w0, w0, 48      // adjust ascii
+    strb w0, [x1, 2]    // store 8 = 01000
+
+    mov w0, 0b010       // 2 = 010
+    lsl w0, w0, 1       // w0 << 1
+    add w0, w0, 48      // adjust ascii
+    strb w0, [x1, 4]    // store 4 = 100
+
+    mov x0, 0b0001      // 1
+    ror x0, x0, 61      // ror 61
+    add w0, w0, 48      // adjust ascii
+    strb w0, [x1, 6]    // store 8 = 1000 
+
+    mov x0, 1           // stdout
+    mov x2, 8           // size
+    mov x8, 64          // write
+    svc 0               // syscall
+
+    mov x0, x1          // return value
+    mov x8, 93          // exit
+    svc 0               // syscall
---
+++ arm64/07_slope.s
@@ -0,0 +1,25 @@
+/*
+
+    Slope of a line
+    Luis Espino 2024
+
+    [./07_slope ; echo $?]
+
+
+*/
+
+.global _start
+
+_start:
+    mov x0, 1           // x1
+    mov x1, 2           // y1
+    mov x3, 3           // x2
+    mov x4, 4           // y2
+
+    sub x5, x4, x1      // y2 - y1
+    sub x6, x3, x0      // x2 - x1
+
+    sdiv x0, x5, x6     // slope
+
+    mov x8, 93          // exit
+    svc 0               // syscall
