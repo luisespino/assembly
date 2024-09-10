017283ac1d2a092d5814b0b618c3320528ed35f2
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-09-09 18:15:46 -0600

    update tabs

--- risc-v/06_and.s
+++ risc-v/06_and.s
@@ -2,9 +2,9 @@
 
 .text
 _start:
-	li t0, 0xFFEEDDCC	# load number to clean
-	li t1, 0x000000FF	# mask to clean left 6 hex
-	and a0, t0, t1		# clean and set in a0
+	li t0, 0xFFEEDDCC   # load number to clean
+	li t1, 0x000000FF   # mask to clean left 6 hex
+	and a0, t0, t1      # clean and set in a0
 	
 	li a7, 93           # exit
 	ecall
--- risc-v/08_shift_left.s
+++ risc-v/08_shift_left.s
@@ -2,8 +2,8 @@
 
 .text
 _start:
-	li t0, 32	    # load number to multiply 32
-	li t1, 2	    # load base of power 2
+	li t0, 32		# load number to multiply 32
+	li t1, 2		# load base of power 2
 	sll a0, t0, t1	# a0 = 32 x 4
 	
 	li a7, 93		# exit
