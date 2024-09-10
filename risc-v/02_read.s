16345d649e5f118ff4b463efd249e3a1a87a26ee
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-09-09 18:20:05 -0600

    update tabs

--- risc-v/02_read.s
+++ risc-v/02_read.s
@@ -5,17 +5,17 @@ buffer: .space 128
 
 .text
 _start:
-	li a0, 0			# load stdout number
-	la a1, buffer		# load buffer address
-	li a2, 128			# load buffer size
-	li a7, 63			# load read number
-	ecall				# syscall
+	li a0, 0		# load stdout number
+	la a1, buffer	# load buffer address
+	li a2, 128		# load buffer size
+	li a7, 63		# load read number
+	ecall			# syscall
 
-	li a0, 1			# load stdin number
-	la a1, buffer		# load buffer address
-	li a2, 128			# load buffer size
-	li a7, 64			# load write number
-	ecall				# syscall
+	li a0, 1		# load stdin number
+	la a1, buffer	# load buffer address
+	li a2, 128		# load buffer size
+	li a7, 64		# load write number
+	ecall			# syscall
 	
-	li a7, 93			# load exit number
-	ecall				# syscall
+	li a7, 93		# load exit number
+	ecall			# syscall
