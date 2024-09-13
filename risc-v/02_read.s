14e278599dd3bdb74eb024356afdffa51f7c9dc1
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-09-13 07:17:31 -0600

    Update 02_read.s

--- risc-v/02_read.s
+++ risc-v/02_read.s
@@ -5,13 +5,13 @@ buffer: .space 128
 
 .text
 _start:
-	li a0, 0        # load stdout number
+	li a0, 0        # load stdin number
 	la a1, buffer   # load buffer address
 	li a2, 128      # load buffer size
 	li a7, 63       # load read number
 	ecall           # syscall
 
-	li a0, 1        # load stdin number
+	li a0, 1        # load stdout number
 	la a1, buffer   # load buffer address
 	li a2, 128      # load buffer size
 	li a7, 64       # load write number
