adbfafa5c206c438da03f5efc1c85f5cca20ac9b
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-05-24 12:00:09 -0600

    Update 01_test.s

--- arm64/01_test.s
+++ arm64/01_test.s
@@ -1,6 +1,7 @@
 .global _start
 
 _start:
-  mov x0, 5     // return value
-  mov x8, 93    // exit syscall_num
-  svc 0         // generic syscall
+    mov x0, 5           // return value
+    mov x8, 93          // exit syscall_num
+    svc 0               // generic syscall
+    
